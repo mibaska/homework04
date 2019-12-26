42da5bdbba610a93ed05743b0f3c6e43c5346faa
Michael Baska <mikebaska1@gmail.com>
2019-12-26 13:55:18 -0600

    quiz screen clears

---
+++ questions.js
@@ -0,0 +1,37 @@
+var questions = [
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    title: "Arrays in JavaScript can be used to store ____.",
+    choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    answer: "all of the above"
+  },
+  {
+    title: "String values must be enclosed within ____ when being assigned to variables.",
+    choices: ["commas", "curly brackets", "parenthesis", "square brackets"],
+    answer: "parenthesis"
+  },
+  {
+    title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+    answer: "console.log"
+  }
+];
+
+var quizTime = questions.length * 15000;
+
+var quizButton = document.querySelector("#quizButton");
+
+quizButton.addEventListener("click", function(event) {
+  event.preventDefault();
+
+  document.getElementById("quizBox").innerHTML = "";
+});
